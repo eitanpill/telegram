cdc8d6f54f541ca2f06f90a96eed12935171d648
Eitan Pelles <eitanpill@gmail.com>
2025-01-23 20:10:59 +0200

    Replaced keep_alive.py with clean version

--- keep_alive.py
+++ keep_alive.py
@@ -1,32 +1,15 @@
 from flask import Flask
 from threading import Thread
-<<<<<<< HEAD
-from os import system
 
-# יצירת Flask App
-=======
-
->>>>>>> 743a9e31a4cade7228b3c6be77045a6715ab4b32
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is running and accessible!"
+    return "Bot is alive!"
 
-<<<<<<< HEAD
-# פונקציה להרצת Flask עם Gunicorn
-def run():
-    system("gunicorn -w 4 -b 0.0.0.0:8080 keep_alive:app")
-
-# פונקציה לשמירה על חיי השרת
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-=======
 def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
->>>>>>> 743a9e31a4cade7228b3c6be77045a6715ab4b32
